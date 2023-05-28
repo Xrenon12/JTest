0cd89225b58130a15372c5fba6d0c376fcf9a13a
Xrenon12 <82339114+Xrenon12@users.noreply.github.com>
2023-05-28 17:37:11 +0300

    Add files via upload

---
+++ Result.py
@@ -0,0 +1,24 @@
+import datetime
+import json
+data = {}
+params = ['sampleCount', 'errorPct', 'medianResTime', 'maxResTime']
+
+with open('D:\Jmeter\LastBuildResult\statistics.json', 'r') as f:
+    d = json.load(f)
+
+    for request in d:
+        if request != 'Requests' and request != 'Total':
+            data[request] = {}
+            for key in d[request]:
+                if key in params:
+                    data[request][key] = d[request][key]
+        elif request == 'Total':
+            data[request] = {}
+            for key in d[request]:
+                if isinstance(d[request][key], float):
+                    data[request][key] = round(d[request][key], 3)
+                else:
+                    data[request][key] = d[request][key]
+file = open("TotalResult.txt", "w+")
+file.write(str(json.dumps(data, indent=4)))
+file.close()
