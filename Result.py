30078288e10c446b107d45df3b80ef4e29e25040
Xrenon12 <82339114+Xrenon12@users.noreply.github.com>
2023-10-05 15:17:23 +0300

    Add files via upload

--- Result.py
+++ Result.py
@@ -10,20 +10,15 @@ except ImportError:
 finally:
     import telebot
 
-# Инициализация переменных
-initiator, start_time, test_run_time, host_name, file_name, stand, count_users, rampart, link, build_number, full = sys.argv
-# initiator='jkl'
-# start_time='kl;'
-# test_run_time='kl;'
-# host_name='kl;'
-# file_name='kl;'
-# stand='kl;'
-# count_users=5
-# rampart=5
-# link='jkl'
-# build_number=149
-# full='false'
-
+test_run_time = os.getenv("DURATION")
+host_name = 'R174'
+file_name = os.getenv("FileName")
+stand = os.getenv("BASE_URL_2")
+count_users = os.getenv("NUMBER_USERS")
+rampart = os.getenv("RAMPART_SEC")
+link = 'link'
+build_number = os.getenv("BUILD_NUMBER")
+full = os.getenv("FullResponse")
 
 bot = telebot.TeleBot('6148942898:AAFfzdCTZNQWvFjaccxtTIrJd7T8rta1Tqo')
 data = {}
@@ -45,89 +40,80 @@ localization = {
     'receivedKBytesPerSec': 'Получено Kb/сек',
     'sentKBytesPerSec': 'Отправлено Kb/сек'
 }
+report_path = 'D:\Jmeter
 
 def get_change(current, previous):
-    if current == previous:
-        return str('+ 0')
-    try:
-        result = round(((current - previous) / previous) * 100, 2)
-        if result >= 0:
-            return str('+' + str(result))
-        else:
-            return str('-' + str(result))
-    except ZeroDivisionError:
-        return str('+ 0')
+        if current == previous:
+            return str('+ 0')
+        try:
+            result = round(((current - previous) / previous) * 100, 2)
+            if result >= 0:
+                return str('+' + str(result))
+            else:
+                return str('-' + str(result))
+        except ZeroDivisionError:
+            return str('+ 0')
 
-# Чтение файла текущего билда
-with open('D:\Jmeter\LastBuildResult\statistics.json', 'r') as f:
-    d = json.load(f)
 
-    for request in d:
-        if request != 'Requests' and request != 'Total':
-            data[request] = {}
-            for key in d[request]:
-                if key in params:
-                    data[request][key] = d[request][key]
-        elif request == 'Total':
-            data[request] = {}
-            for key in d[request]:
-                if isinstance(d[request][key], float):
-                    data[request][key] = round(d[request][key], 3)
-                else:
-                    data[request][key] = d[request][key]
-
-# Чтение файла предыдущего билда
-    with open(f'D:\Jmeter\DashBoard{str(int(build_number)-1)}\statistics.json', 'r') as f:
+    # Чтение файла текущего билда
+    with open(f'{report_path}\LastBuildResult\statistics.json', 'r') as f:
         d = json.load(f)
 
         for request in d:
             if request != 'Requests' and request != 'Total':
-                old_build[request] = {}
+                data[request] = {}
                 for key in d[request]:
                     if key in params:
-                        old_build[request][key] = d[request][key]
+                        data[request][key] = d[request][key]
             elif request == 'Total':
-                old_build[request] = {}
+                data[request] = {}
                 for key in d[request]:
                     if isinstance(d[request][key], float):
-                        old_build[request][key] = round(d[request][key], 3)
+                        data[request][key] = round(d[request][key], 3)
                     else:
-                        old_build[request][key] = d[request][key]
+                        data[request][key] = d[request][key]
 
-text_title = f'G1 Jmeter\n \nData: {datetime.datetime.now()} \n\n' \
-        f'Start time: {start_time}\n' \
-        f'Test run time (sec): {test_run_time}\n' \
-        f'Host name: {host_name}\n' \
-        f'file name: {file_name}\n' \
-        f'Stand: {stand}\n' \
-        f'Count users: {count_users}\n' \
-        f'Rampart (sec): {rampart}\n' \
-        f'Link: {link}\n\n'
-text = ''
-if full == 'false':
-    for i in data['Total']:
-        if i != 'transaction':
-            text += localization[i] + ' - ' + str(round(data['Total'][i], 3)) + ' (' + str(get_change(float(data['Total'][i]), float(old_build['Total'][i]))) + '%),\n'
-else:
-    for i in data:
-        if i != 'Total':
-            text += '\nTransaction: ' + i + '\n'
-            for j in data[i]:
-                try:
-                    message_part = localization[j] + ' - ' + str(round(data[i][j], 3)) + ' (' + str(get_change(float(data[i][j]), float(old_build[i][j]))) + '%),\n'
-                    text += message_part
-                except:
-                    message_part = localization[j] + ' - ' + str(round(data[i][j], 3)) + '\n'
-                    text += message_part
-img = open('D:/Jmeter/gra/gra-ResponseTimesOverTime.png', 'rb')
+        # Чтение файла предыдущего билда
+        with open(f'{report_path}\DashBoard{str(int(build_number) - 1)}\statistics.json', 'r') as f:
+            d = json.load(f)
 
-if len(text) > 4096:
-    bot.send_photo(5107055135, img, caption=text_title)
-    for x in range(0, len(text), 4096):
-        bot.send_message(5107055135, text[x:x+4096])
-else:
-    if(len(text+text_title)>1024):
-        bot.send_photo(5107055135, img, caption=text_title)
-        bot.send_message(5107055135, text)
+            for request in d:
+                if request != 'Requests' and request != 'Total':
+                    old_build[request] = {}
+                    for key in d[request]:
+                        if key in params:
+                            old_build[request][key] = d[request][key]
+                elif request == 'Total':
+                    old_build[request] = {}
+                    for key in d[request]:
+                        if isinstance(d[request][key], float):
+                            old_build[request][key] = round(d[request][key], 3)
+                        else:
+                            old_build[request][key] = d[request][key]
+
+    text_title = f'Gandiva Jmeter\n \nData: {datetime.datetime.now()} \n\n' \
+                 f'Test run time (sec): {test_run_time}\n' \
+                 f'Host name: {host_name}\n' \
+                 f'file name: {file_name}\n' \
+                 f'Stand: {stand}\n' \
+                 f'Count users: {count_users}\n' \
+                 f'Rampart (sec): {rampart}\n\n' \
+                 f'<a href="{link}">Jmeter results</a>\n\n'
+    text = ''
+    if full == 'false':
+        for i in data['Total']:
+            if i != 'transaction':
+                text += localization[i] + ' - ' + str(round(data['Total'][i], 3)) + ' (' + str(get_change(float(data['Total'][i]), float(old_build['Total'][i]))) + '%),\n'
     else:
-        bot.send_photo(5107055135, img, caption=text_title+text)
+        for i in data:
+            if i != 'Total':
+                text += '\nTransaction: ' + i + '\n'
+                for j in data[i]:
+                    try:
+                        message_part = localization[j] + ' - ' + str(round(data[i][j], 3)) + ' (' + str(get_change(float(data[i][j]), float(old_build[i][j]))) + '%),\n'
+                        text += message_part
+                    except:
+                        message_part = localization[j] + ' - ' + str(round(data[i][j], 3)) + '\n'
+                        text += message_part
+
+bot.send_message(5107055135, text)
